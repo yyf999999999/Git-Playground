a134c9adcdce7c96ff462e7c50b6f17b1f171fc4
Kill <10919128+KB2Maker@users.noreply.github.com>
2023-10-16 10:32:57 +0900

    ボブが機能Bのスタブを作成

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import math
-
+import datetime
 # [機能A]@アリス担当 #############
 
 
 # [機能B]@ボブ担当 ###############
-
+def func_B ():
+  print('func_B called.')
 
 # メイン処理 #####################
 if __name__ == "__main__": 
@@ -14,5 +15,5 @@ if __name__ == "__main__":
   ## 機能Aの実行
 
   ## 機能Bの実行
-
+  func_B()
   print("end.")
