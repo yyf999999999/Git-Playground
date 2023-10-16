ef68b4ad3846ffb6411982f8050f5c6717ac7ef7
Kill <10919128+KB2Maker@users.noreply.github.com>
2023-10-16 10:33:53 +0900

    ボブが機能Bを実装

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ import datetime
 
 # [機能B]@ボブ担当 ###############
 def func_B ():
-  print('func_B called.')
+  t1 = datetime.datetime.now() # 現在時刻
+  t2 = datetime.datetime(2023,11,4,10,00,00) # 11/4 10:00
+  diff = t2 - t1
+  days = diff.days
+  hours= diff.seconds // 3600
+  print('高専祭まで、あと',end='')
+  print(days,end='')
+  print('日と',end='')
+  print(hours,end='')
+  print('時間')
 
 # メイン処理 #####################
 if __name__ == "__main__": 
